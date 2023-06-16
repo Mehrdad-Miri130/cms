37e16cc60639796b5c11229517cb5ec76a6e6723
Mehrdad Miri <83897879+Mehrdad-Miri130@users.noreply.github.com>
2023-06-16 23:27:40 +0430

    add new validation

--- server/router/pages.js
+++ server/router/pages.js
@@ -13,25 +13,51 @@ router
       check("title").notEmpty().isString(),
       check("author").notEmpty().isNumeric(),
       check("content").notEmpty().isString(),
-      check("publishedAt").isDate({ format: "YYYY-MM-DD", strictMode: true }),
+      check("publishedAt").isString(),
     ],
     controller.addPageByAdmin
   );
 
 router
   .route("/by-admin/:id")
-  .patch(auth.isLoggedIn, auth.restrictToAdmin, controller.updatePageByAdmin)
+  .patch(
+    auth.isLoggedIn,
+    auth.restrictToAdmin,
+    [
+      check("title").notEmpty().isString(),
+      check("author").notEmpty().isNumeric(),
+      check("content").notEmpty().isString(),
+      check("publishedAt").isString(),
+    ],
+    controller.updatePageByAdmin
+  )
   .delete(auth.isLoggedIn, auth.restrictToAdmin, controller.deleteOneByAdmin);
 
 router
   .route("/")
   .get(controller.getAllPages)
-  .post(auth.isLoggedIn, controller.addPage);
+  .post(
+    auth.isLoggedIn,
+    [
+      check("title").notEmpty().isString(),
+      check("content").notEmpty().isString(),
+      check("publishedAt").isString(),
+    ],
+    controller.addPage
+  );
 
 router
   .route("/:id")
   .get(controller.getOne)
-  .patch(auth.isLoggedIn, controller.updatePageByUser)
+  .patch(
+    auth.isLoggedIn,
+    [
+      check("title").notEmpty().isString(),
+      check("content").notEmpty().isString(),
+      check("publishedAt").isString(),
+    ],
+    controller.updatePageByUser
+  )
   .delete(auth.isLoggedIn, controller.deleteOne);
 
 module.exports = router;
