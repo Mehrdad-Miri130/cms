1f2e0ee713a20e25205db7c229585cda5dff6043
Mehrdad Miri <83897879+Mehrdad-Miri130@users.noreply.github.com>
2023-06-19 23:39:41 +0330

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(morgan("dev"));
 //app.use(cors(corsOptions));
 app.use(cors({
   "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "methods": "*",
   "preflightContinue": false,
   "credentials": true,
   "optionsSuccessStatus": 200
